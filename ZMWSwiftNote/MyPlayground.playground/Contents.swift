f8d067668080ff4794f7183561aaa7b1f9d86e5a
xiaoming <zjjzmw1@163.com>
2016-08-18 19:42:17 +0800

    添加几个测试的playground

--- ZMWSwiftNote/MyPlayground.playground/Contents.swift
+++ ZMWSwiftNote/MyPlayground.playground/Contents.swift
@@ -4,5 +4,17 @@ import UIKit
 
 var str = "Hello, playground"
 
+var sum = 0
+for a in 0 ... 10 {
+    sum += a
+}
+for b in 0 ... 10 {
+    sum += 1
+}
+for c in 1 ... 10 {
+    sum = sum * 2
+}
+
+
 
 
